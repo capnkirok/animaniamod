af5cc1ad7d8dfccd00e9d52ea80b8a25b4b63a4c
Purplicious_Cow <dbasden@gmail.com>
2017-06-18 08:02:08 +0200

    Patreon Update
    Updated patreon list

--- src/main/java/com/animania/common/handler/PatreonHandler.java
+++ src/main/java/com/animania/common/handler/PatreonHandler.java
@@ -25,15 +25,13 @@ public class PatreonHandler
 		patreonUUIDS.add("9c8a434b-2adc-4385-bc58-a8f08db3ebb9"); //TheJurassicAlien
 		patreonUUIDS.add("1fbb6b43-af8f-40a1-9c26-c5128ff513ce"); //vroulas
 		patreonUUIDS.add("fdd172ed-dcb5-49ed-93ae-7847f6e88bef"); //Tschipp
-
 		
 		//PATREONS
 		patreonUUIDS.add("b0bb3fff-ddbd-40c6-95d7-51dfcaece879"); //LeKoopa92
 		patreonUUIDS.add("1f471396-84d9-41a0-ad9b-52a722c12a6a"); //stacyplays
 		patreonUUIDS.add("407720e3-22a6-4a78-9e64-0f6e82e00609"); //Xorbah
 		patreonUUIDS.add("f865dbb5-552c-4f62-8d73-2360634f6edd"); //Gold172
-		
-		
+		patreonUUIDS.add("2ea31456-53d4-4eee-b034-5126fc45e1c7"); //akirakujo
 
 	}
 	
