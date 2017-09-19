3539019ff95ab7d7573a8514fa37a889be1f4e1c
Purplicious_Cow <dbasden@gmail.com>
2017-09-20 01:41:20 +0200

    Spawning Nerfs, Goats Faint

--- src/main/java/com/animania/common/entities/goats/EntityBuckFainting.java
+++ src/main/java/com/animania/common/entities/goats/EntityBuckFainting.java
@@ -2,6 +2,8 @@ package com.animania.common.entities.goats;
 
 import com.animania.common.handler.ItemHandler;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public class EntityBuckFainting extends EntityBuckBase
@@ -27,6 +29,22 @@ public class EntityBuckFainting extends EntityBuckBase
 	public int getSecondaryEggColor()
 	{
 		return 14803425;
+	}
+	
+	@Override
+	protected void collideWithEntity(Entity entityIn)
+	{
+
+		if (entityIn instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) entityIn;
+			if (player.isSprinting())
+			{			
+				this.setSpooked(true);
+				this.setSpookedTimer(1.0F);
+				
+			}
+			entityIn.applyEntityCollision(this);
+		}
 	}
 
 }
