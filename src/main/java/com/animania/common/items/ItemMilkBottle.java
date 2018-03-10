8f5e6ccd31e7b2ed535824dd7535f9cf5787d421
Purplicious_Cow <dbasden@gmail.com>
2018-03-10 18:34:14 +0100

    Revert "deleting files"
    This reverts commit e6c1ad0579155c7fb5a8357c599d5b5fdbb8db99.

--- src/main/java/com/animania/common/items/ItemMilkBottle.java
+++ src/main/java/com/animania/common/items/ItemMilkBottle.java
@@ -4,35 +4,20 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import com.animania.Animania;
-import com.animania.common.helper.RomanNumberHelper;
-import com.animania.common.helper.TimeHelper;
-import com.animania.config.AnimaniaConfig;
-
-import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
-import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemMilkBottle extends ItemAnimaniaFood
 {
@@ -56,26 +41,15 @@ public class ItemMilkBottle extends ItemAnimaniaFood
 				EntityItem entityitem = new EntityItem(worldIn, entityLiving.posX + 0.5D, entityLiving.posY + 0.5D, entityLiving.posZ + 0.5D, new ItemStack(Items.GLASS_BOTTLE));
 				worldIn.spawnEntity(entityitem);
 			}
-			
+
 			EntityPlayer entityplayer = (EntityPlayer)entityLiving;
 			entityplayer.getFoodStats().addStats(this, stack);
 			worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
 			this.onFoodEaten(stack, worldIn, entityplayer);
 			entityplayer.addStat(StatList.getObjectUseStats(this));
-
-			if (entityplayer instanceof EntityPlayerMP)
-			{
-				CriteriaTriggers.CONSUME_ITEM.trigger((EntityPlayerMP)entityplayer, stack);
-			}
 		}
 
 		return stack;
-	}
-
-	@Override
-	protected void onFoodEaten(ItemStack itemstack, World worldObj, EntityPlayer entityplayer)
-	{
-
 	}
 
 	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
@@ -85,11 +59,11 @@ public class ItemMilkBottle extends ItemAnimaniaFood
 	}
 
 	@Override
-	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+	public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean advanced)
 	{
 
-		tooltip.add(TextFormatting.GREEN + I18n.translateToLocal("tooltip.an.removeall"));
-		tooltip.add(TextFormatting.GOLD + I18n.translateToLocal("tooltip.an.edibleanytime"));
+		list.add(TextFormatting.GREEN + I18n.translateToLocal("tooltip.an.removeall"));
+		list.add(TextFormatting.GOLD + I18n.translateToLocal("tooltip.an.edibleanytime"));
 
 	}
 }
