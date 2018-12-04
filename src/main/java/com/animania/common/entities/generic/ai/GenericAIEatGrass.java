625429539d12ada95afa86f32a03b0dc41c634ff
Thomas Rohloff <v10lator@myway.de>
2018-12-04 13:12:24 +0100

    Wire ReflectionUtil into GenericAIEatGrass
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main/java/com/animania/common/entities/generic/ai/GenericAIEatGrass.java
+++ src/main/java/com/animania/common/entities/generic/ai/GenericAIEatGrass.java
@@ -1,12 +1,13 @@
 package com.animania.common.entities.generic.ai;
 
+import org.apache.logging.log4j.LogManager;
+
 import com.animania.api.interfaces.IFoodEating;
 import com.animania.api.interfaces.ISleeping;
 import com.animania.common.entities.cows.CowType;
 import com.animania.common.entities.cows.EntityAnimaniaCow;
+import com.animania.common.helper.ReflectionUtil;
 import com.animania.config.AnimaniaConfig;
-import com.corosus.desirepaths.DesirePaths;
-import com.corosus.desirepaths.block.BlockGrassWorn;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
@@ -128,10 +129,21 @@ public class GenericAIEatGrass<T extends EntityCreature & ISleeping & IFoodEatin
 
 					if (AnimaniaConfig.gameRules.plantsRemovedAfterEating)
 					{
-
-						if (Loader.isModLoaded("desirepaths") && ((block instanceof BlockGrassWorn && block != DesirePaths.dirt_6) || block instanceof BlockGrass))
-							BlockGrassWorn.performWearTick(entityWorld, this.seekingBlockPos, 20F);
-						else
+						String name = block.getRegistryName().toString();
+						boolean handled = false;
+						
+						if (Loader.isModLoaded("desirepaths") && ((name.contains("desirepaths:grass_worn_") && !name.endsWith("6")) || block instanceof BlockGrass))
+						{
+							try {
+								ReflectionUtil.findMethod(Class.forName("com.corosus.desirepaths.block.BlockGrassWorn"), "performWearTick", null, World.class, BlockPos.class, float.class).invoke(null, this.entityWorld, this.seekingBlockPos, 20.0F);
+								handled = true;
+							} catch (Exception e) {
+								// Do nothing as handled is still false
+								e.printStackTrace();
+							}
+						}
+						
+						if(!handled)
 							this.entityWorld.setBlockState(this.seekingBlockPos, Blocks.DIRT.getDefaultState(), 2);
 					}
 
