3e7da6875fcabbcd4841692ab29b8b1fc47f8349
Tschipp <mrtschipp@gmail.com>
2021-02-19 15:53:22 +0100

    Added herding behavior

---
+++ src/main/java/com/animania/addons/catsdogs/common/entity/canids/ai/AnimalAIGetDogHerded.java
@@ -0,0 +1,65 @@
+package com.animania.addons.catsdogs.common.entity.canids.ai;
+
+import java.util.List;
+
+import com.animania.api.interfaces.ISleeping;
+import com.animania.common.helper.AnimaniaHelper;
+
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.pathfinding.Path;
+import net.minecraft.pathfinding.PathNavigate;
+import net.minecraft.pathfinding.PathPoint;
+
+public class AnimalAIGetDogHerded<T extends EntityCreature & ISleeping, U extends EntityTameable & ISleeping> extends EntityAIBase
+{
+	private final T herdAnimal;
+	private final Class<? extends U> herderType;
+
+	public AnimalAIGetDogHerded(T entity, Class<? extends U> herderType)
+	{
+		this.herdAnimal = entity;
+		this.herderType = herderType;
+		this.setMutexBits(3);
+	}
+
+	@Override
+	public boolean shouldExecute()
+	{
+		if (herdAnimal.getSleeping())
+			return false;
+
+		List<U> list = AnimaniaHelper.getEntitiesInRangeWithPredicate(herderType, 10, herdAnimal.world, herdAnimal, e -> !e.getSleeping() && e.isTamed() && !e.isSitting());
+		if (list.isEmpty())
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public boolean shouldContinueExecuting()
+	{
+		if (!shouldExecute())
+			return false;
+
+		return super.shouldContinueExecuting();
+	}
+
+	@Override
+	public void updateTask()
+	{
+		List<U> list = AnimaniaHelper.getEntitiesInRangeWithPredicate(herderType, 10, herdAnimal.world, herdAnimal, e -> !e.getSleeping() && e.isTamed() && !e.isSitting());
+		if (list.size() > 0)
+		{
+			U herder = list.get(0);
+			PathNavigate herderNav = herder.getNavigator();
+			Path herderPath = herderNav.getPath();
+			if (herderPath != null)
+			{
+				PathPoint herderDest = herderPath.getFinalPathPoint();
+				herdAnimal.getNavigator().tryMoveToXYZ(herderDest.x, herderDest.y, herderDest.z, 1.0f);
+			}
+		}
+	}
+}
